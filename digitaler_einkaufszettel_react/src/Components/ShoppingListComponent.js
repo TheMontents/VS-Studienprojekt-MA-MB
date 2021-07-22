83a8e2b15b1937a45df97c021310f10866d22f27
Marcel Autenrieth <maauit02@hs-esslingen.de>
2021-07-22 15:45:32 +0200

    improved ui and added dockerfile to react app

--- digitaler_einkaufszettel_react/src/Components/ShoppingListComponent.js
+++ digitaler_einkaufszettel_react/src/Components/ShoppingListComponent.js
@@ -45,33 +45,38 @@ class ShoppingListComponent extends React.Component{
     render (){
         return(
             <div>
-                <div>
-                    <form onSubmit={this.handleSubmit}>
-                        <input type="text" placeholder="Einkaufszettel Eintrag" value={this.state.newShoppingListEntry} onChange={this.handleInputChange}/>
-                        <button type="submit" >Hinzufügen</button>
-                    </form>
+                <div className="shadow card m-3">
+                <h5 className="card-header text-white bg-secondary">Artikel hinzufügen</h5>
+                    <div className="card-body">
+                        <form onSubmit={this.handleSubmit}>
+                            <div className="input-group mb-3">
+                                <input type="text" className="form-control" placeholder="Einkaufszettel Eintrag" value={this.state.newShoppingListEntry} onChange={this.handleInputChange}/>
+                                <div className="input-group-append">
+                                    <button type="submit" className="btn btn-secondary">Hinzufügen</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
                 </div>  
                 <div>
-                    <h1 className = "text-center"> Einkaufszettel</h1>
-                    <table className = "table table-striped">
-                        <thead>
-                            <tr>
-                                <td>Artikel</td>
-                                <td></td>
-                            </tr>
-                        </thead>
+                    <div className="shadow card m-3">
+                        <h5 className="card-header text-white bg-secondary">Einkaufszettel</h5>
+                        <div className="card-body">
+                        <table className = "table table-striped">
                         <tbody>
                             {
                                 this.state.shoppingList.map(
                                     shoppingList => 
                                     <tr key = {shoppingList.articleId}>
                                         <td> {shoppingList.article}</td> 
-                                        <td><button onClick={() => this.deleteArticle(shoppingList.articleId)}>gekauft</button></td>     
+                                        <td><button className="btn btn-secondary" onClick={() => this.deleteArticle(shoppingList.articleId)}>gekauft</button></td>     
                                     </tr>
                                 )
                             }
                         </tbody>
                     </table>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
